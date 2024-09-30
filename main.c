6fd64bfba17076a7c86de205925a904b9203e533
Walieddiner <rajwalieddine@gmail.COM>
2024-09-30 12:54:19 +0100

    calcule

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main(){
+   int n;
+    printf("1.Addition : Ajouter deux ou plusieurs nombres.\n2.Soustraction : Soustraire deux nombres.\n3.Multiplication : Multiplier deux ou plusieurs nombres.\n4.Division : Diviser deux nombres, avec gestion de la division par zéro.\n5.Moyenne : Calculer la moyenne d'une série de nombres.\n6.Valeur absolue : Calculer la valeur absolue d'un nombre.\n7.Exponentiation : Calculer un nombre à une certaine puissance.\n8.Racine carrée : Calculer la racine carrée d'un nombre positif.");
+    scanf("%d",&n);
+
+    if (n==1||n==3){
+        printf("")
+    }
+}
+
+
